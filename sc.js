60f7037b3af18ddb7ac4004e4610b41c848ba451
Shibjyoti-src <139529315+shib-weber@users.noreply.github.com>
2024-05-09 23:00:04 +0530

    more

--- sc.js
+++ sc.js
@@ -1,16 +1,18 @@
 const a=document.querySelector(".cc");
 const b=document.querySelector(".button1");
 const c=document.querySelector(".button2");
-const wp= a.getBoundingClientRect();
+const d=document.querySelector(".container");
+const wp= d.getBoundingClientRect();
 const nb= c.getBoundingClientRect();
 
 b.addEventListener("click",()=>{
 })
 c.addEventListener("mouseover",()=>{
-    const i=Math.floor(Math.random()*(wp.width - nb.width))+1;
-    const j=Math.floor(Math.random()*(wp.height - nb.height))+1;
+    const i=Math.floor(Math.random()*300)+1;
+    const j=Math.floor(Math.random()*300)+1;
     console.log(i);
     console.log(j);
-    c.style.left = i +"px"
-    c.style.top = j +"px"
+
+    d.style.top = i +"px"
+    d.style.left= j +"px"
 })
